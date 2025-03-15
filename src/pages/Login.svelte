9b396b68bbd84ab114fcf21e32a41521dbf8c5b1
soohofather <enthchal@gmail.com>
2025-03-15 20:57:37 +0900

    71강까지

---
+++ src/pages/Login.svelte
@@ -0,0 +1,20 @@
+<script
+  src="https://cdnjs.cloudflare.com/ajax/libs/js-sha256/0.9.0/sha256.min.js"
+></script>
+
+<form id="login-form">
+  <div>로그인</div>
+  <div>
+    <label for="id">아이디</label>
+    <input type="text" id="id" name="id" required />
+  </div>
+  <div>
+    <label for="password">비밀번호</label>
+    <input type="password" id="password" name="password" required />
+  </div>
+  <div>
+    <button type="submit">로그인</button>
+  </div>
+  <div id="password-info"></div>
+  <div id="login-info"></div>
+</form>
---
+++ src/pages/Main.svelte
@@ -0,0 +1,20 @@
+<script>
+  let hour = new Date().getHours();
+  let min = new Date().getMinutes();
+
+  setInterval(() => (min = min + 1), 1000);
+</script>
+
+<header>
+  <div class="info-bar__time">{hour}:{min}</div>
+</header>
+<main>
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+<footer></footer>
+
+<style>
+  .info-bar__time {
+    color: blue;
+  }
+</style>
